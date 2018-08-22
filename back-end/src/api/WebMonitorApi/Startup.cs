078c1a881d8b387edf8b967800e28407b29bc01a
Azamat Sadr <sadr_17@mil.ru>
2018-08-22 11:36:06 +0300

    Fixed format of file

--- back-end/src/api/WebMonitorApi/Startup.cs
+++ back-end/src/api/WebMonitorApi/Startup.cs
@@ -1,33 +1,48 @@
-using Microsoft.AspNetCore.Builder; using Microsoft.AspNetCore.Cors.Infrastructure; using Microsoft.AspNetCore.Hosting; using 
-Microsoft.AspNetCore.HttpOverrides; using Microsoft.EntityFrameworkCore; using Microsoft.Extensions.Configuration; using 
-Microsoft.Extensions.DependencyInjection; using Monitor; using WebMonitorApi.Broadcast; using WebMonitorApi.Hubs; using 
-WebMonitorApi.Models; using WebMonitorApi.Repository;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Monitor;
+using WebMonitorApi.Broadcast;
+using WebMonitorApi.Hubs;
+using WebMonitorApi.Models;
+using WebMonitorApi.Repository;
 
 namespace WebMonitorApi {
+
   /// <summary>
   /// Web API startup class.
   /// </summary>
   public class Startup
   {
     public IConfiguration Configuration { get; }
+
     public string ConnectionString => Configuration.GetSection("Data:WebPagesContext:ConnectionString")?.Value;
+
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+
       services.AddSignalR()
         .AddHubOptions<UpdateStatusHub>((hub) =>
         {
           hub.EnableDetailedErrors = true;
         });
+
       var corsBuilder = new CorsPolicyBuilder();
       corsBuilder.AllowAnyHeader();
       corsBuilder.AllowAnyMethod();
       corsBuilder.AllowAnyOrigin();
       corsBuilder.AllowCredentials();
+
       services.AddCors(options =>
       {
         options.AddPolicy("SiteCorsPolicy", corsBuilder.Build());
       });
+
       services.AddEntityFrameworkNpgsql()
         .AddDbContext<WebPagesContext>(options =>
         {
@@ -37,18 +52,21 @@ namespace WebMonitorApi {
       services.AddScoped<IRepository<WebPage>, WebPages>();
       services.AddSingleton<WebPagesMonitor>();
     }
+
     public void Configure(IApplicationBuilder app, IHostingEnvironment env)
     {
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
       }
+
       app.UseCors("SiteCorsPolicy");
 
       app.UseForwardedHeaders(new ForwardedHeadersOptions
       {
         ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
       });
+
       app.UseSignalR(routes =>
       {
         routes.MapHub<UpdateStatusHub>("/updateHub");
